284b94d7beb431cbad0f599480944f2dcf2ea395
guillex <guillex@lizardfs.org>
2021-08-03 19:21:01 -0400

    tests: Improve python formatting using mypy and black
    Adding return types to functions. Adding double quotes instead of
    single quotes.
    
    Change-Id: Iec2569411c4222216822612d2ee8550329b1b12d

--- tests/dispatcher/client/next_test.py
+++ tests/dispatcher/client/next_test.py
@@ -7,10 +7,10 @@ URL = "http://127.0.0.1:5000/next_test"
 if len(sys.argv) == 2:
     pipeline_id = sys.argv[1]
 else:
-    pipeline_id = 1
+    pipeline_id = "1"
 
-PARAMS = {'pipeline_id':pipeline_id}
+PARAMS = {"pipeline_id": pipeline_id}
 
-response = requests.get(url = URL, params = PARAMS)
+response = requests.get(url=URL, params=PARAMS)
 
 print(response.text)
