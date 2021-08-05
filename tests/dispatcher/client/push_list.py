aaf50ab2d9c2dda4993a4be252365a693b6d74fa
Baldor <baldor@lizardfs.org>
2021-08-04 23:15:17 -0400

    tests: Parse arguments on push_list script
    By passing the actual arguments, the mocked data is no necessary anymore so it is also removed
    
    Change-Id: I5f47d3757a6b9632c08dbd34cd8433bfe6c738bf

--- tests/dispatcher/client/push_list.py
+++ tests/dispatcher/client/push_list.py
@@ -1,21 +1,14 @@
+import argparse
 import os
 import sys
+from argparse import Namespace
 
 import requests
 
 from util import slash_join, get_tests_list_with_durations
 
 TESTS_DISPATCHER_URL = os.environ.get("TESTS_DISPATCHER_URL", "http://127.0.0.1:5000/")
-URL = slash_join(TESTS_DISPATCHER_URL, "push_list")
 
-if len(sys.argv) == 3:
-    pipeline_id = sys.argv[1]
-    tests = sys.argv[2]
-else:
-    pipeline_id = "1"
-    tests = "test01:test02:test03:test04"
-
-PARAMS = {"pipeline_id": pipeline_id, "tests": tests}
 
 
 def push_list() -> str:
@@ -25,4 +18,47 @@ def push_list() -> str:
 
 
 if __name__ == "__main__":
-    push_list()
+    """Script which is used to split (all minus manually_excluded) tests from a given test_suite
+    into 'nodes_count' groups, so that tests from each group can be run concurrently.
+    Goal for the partition is that tests from each group should run for approximately
+    the same duration.
+    Approximate duration of each test is stored in and fetched from tests/data/'test_suite'.txt"""
+
+    parser = argparse.ArgumentParser(
+        description="Filter tests for concurrent runs. All following arguments besides 'excluded_tests' are required."
+    )
+    parser.add_argument(
+        "-a", "--action", type=str, help="The action to request from the dispatcher\n"
+                                         "Currently:\n"
+                                         " - push_list\n"
+                                         " - next_test\n"
+    )
+    parser.add_argument(
+        "-b", "--build_id", type=str, help="Id of the current build"
+    )
+    parser.add_argument(
+        "-w", "--workspace", type=str, help="Path to lizardfs test directory"
+    )
+    parser.add_argument("-s", "--test_suite", type=str, help="Name of test_suite")
+    parser.add_argument(
+        "-e",
+        "--excluded_tests",
+        type=str,
+        help="Names of excluded tests, separated by ':'",
+    )
+
+    args = parser.parse_args()
+
+    if (
+            not args.build_id
+            or not args.workspace
+            or not args.test_suite
+            or not args.action
+    ):
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    if args.action == 'push_list':
+        push_list(args)
+    elif args.action == 'next_test':
+        next_test(args)
