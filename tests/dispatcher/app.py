8f23caf36a816e3d2ad675e1a1b1cf17439ad92e
Baldor <baldor@lizardfs.org>
2021-08-06 07:22:43 -0400

    tests: Add json format and http status code to the dispatcher's responses
    Change-Id: Idef579d68c3d7cf3934d36ea1952ebc5a7f85934

--- tests/dispatcher/app.py
+++ tests/dispatcher/app.py
@@ -1,7 +1,7 @@
-from typing import Dict, List
-
+import json
 from flask import Flask
-from flask import request
+from flask import request, Response
+from typing import Dict, List
 
 
 def create_app() -> Flask:
@@ -9,13 +9,12 @@ def create_app() -> Flask:
 
     app.data: Dict[str:Dict[str:List[str]]] = {}
 
-    @app.route("/")
-    def index() -> str:
-        print(app.data)
-        return str(app.data)
+    @app.route("/", methods=["GET"])
+    def index() -> Response:
+        return Response(json.dumps(app.data), status=200, mimetype='application/json')
 
     @app.route("/push_list", methods=["POST"])
-    def push_list() -> dict:
+    def push_list() -> Response:
         json_data = request.json
         build_id = json_data.get("build_id")
         test_suite = json_data.get("test_suite")
@@ -23,30 +22,26 @@ def create_app() -> Flask:
 
         if build_id in app.data:
             if test_suite in app.data[build_id]:
-                return {"error": "This pipeline id already exists."}
+                return Response(json.dumps({"details": "This tests list exists already."}), status=412,
+                                mimetype='application/json')
         else:
             app.data[build_id] = {}
 
         app.data[build_id][test_suite] = tests
 
-        return {"status": "OK"}
+        return Response(json.dumps({"details": "OK"}), status=201, mimetype='application/json')
 
     @app.route("/next_test", methods=["GET"])
-    def next_test() -> str:
+    def next_test() -> Response:
         build_id = str(request.args.get("build_id", ""))
         test_suite = str(request.args.get("test_suite", ""))
 
         if build_id == "" or test_suite == "":
-            return ""
-
-        if build_id not in app.data:
-            return ""
-
-        if test_suite not in app.data[build_id]:
-            return ""
-
+            return Response(json.dumps({"details": ""}), status=400, mimetype='application/json')
+        if build_id not in app.data or test_suite not in app.data[build_id]:
+            return Response(json.dumps({"details": ""}), status=404, mimetype='application/json')
         if len(app.data[build_id][test_suite]) == 0:
-            return ""
+            return Response(json.dumps({"details": ""}), status=200, mimetype='application/json')
 
         first_test = app.data[build_id][test_suite][0]
 
@@ -57,6 +52,6 @@ def create_app() -> Flask:
             if len(app.data[build_id]) == 0:
                 del app.data[build_id]
 
-        return first_test
+        return Response(json.dumps({"details": first_test}), status=200, mimetype='application/json')
 
     return app
